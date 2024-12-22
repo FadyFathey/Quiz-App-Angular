c914d4768e8c642e6eb344c9415dae1244d4a5ac
devfadyfathy <fadyfathymaher3@gmail.com>
2024-12-22 08:03:58 +0200

    project initialization and setup

---
+++ src/app/quiz/components/question/question.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { AnswerComponent } from '../answer/answer.component';
+
+
+@Component({
+  selector: 'app-question',
+  standalone: true,
+  imports: [AnswerComponent],
+  templateUrl: './question.component.html',
+  styleUrl: './question.component.css'
+})
+export class QuestionComponent {
+
+}
